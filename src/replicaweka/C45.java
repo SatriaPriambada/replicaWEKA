8f6e26e729155e8fd18ce33c28f1a8c66bfb50fc
SatriaPriambada <satriapriambada@gmail.com>
2016-10-01 10:44:40 +0700

    Init Commit

---
+++ src/replicaweka/C45.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package replicaweka;
+
+/**
+ *
+ * @author Satria
+ */
+public class C45 {
+    public void createModel(){
+        System.out.println("Creating model for C45");
+    }
+}
---
+++ src/replicaweka/ID3.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package replicaweka;
+
+/**
+ *
+ * @author Satria
+ */
+public class ID3 {
+    public void createModel(){
+        System.out.println("Creating model for ID3");
+    }
+    
+}
